a74a4c7877541031a94ccdc9d8a15f0f016b239d
manzhaimaksim <maxbbb88@gmail.com>
2020-05-18 21:48:21 +0300

    edit regexp for validation username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
   EMAIL_REGEXP = /\A.+@.+\z/
-  USERNAME_REGEXP = /\w/
+  USERNAME_REGEXP = /\A[\w]+\z/i
 
   attr_accessor :password
 
