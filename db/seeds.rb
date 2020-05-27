39e119c2a16f106b4a943d1c26b8509ef39dfd6a
manzhaimaksim <maxbbb88@gmail.com>
2020-05-27 22:01:04 +0300

    adding to view color od avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 Question.destroy_all
 User.destroy_all
 
-neo = User.create(id: 1, name: 'Новичок', username: 'Neo', email: 'neo@matrix.com', password: '12345', avatar_url: 'Neo.jpg')
-trinity = User.create(id: 2, name: 'Троица', username: 'Trinity', email: 'trinity@matrix.com', password: '12345', avatar_url: 'Trinityfull.jpg')
-morpheus = User.create(id: 3, name: 'Морфей', username: 'Morpheus', email: 'morpheus@matrix.com', password: '12345', avatar_url: 'Morpheus1.jpg')
+neo = User.create(id: 1, name: 'Новичок', username: 'Neo', email: 'neo@matrix.com', password: '12345', avatar_url: 'Neo.jpg', avatar_color: 'yellow')
+trinity = User.create(id: 2, name: 'Троица', username: 'Trinity', email: 'trinity@matrix.com', password: '12345', avatar_url: 'Trinityfull.jpg', avatar_color: 'green')
+morpheus = User.create(id: 3, name: 'Морфей', username: 'Morpheus', email: 'morpheus@matrix.com', password: '12345', avatar_url: 'Morpheus1.jpg', avatar_color: 'red')
 
 Question.create(user_id: morpheus.id, text: 'Как дела?', created_at: Date.parse('21.05.2020'))
 
