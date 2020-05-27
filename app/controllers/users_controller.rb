c68159962026dfa16213c006f768bd11fddb16b9
manzhaimaksim <maxbbb88@gmail.com>
2020-05-27 22:13:02 +0300

    add editing avatar color to user settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url, :avatar_color)
   end
 end
