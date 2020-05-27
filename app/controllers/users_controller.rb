8184df898346b5ef8ca69c084ad3dd69ff7786c6
manzhaimaksim <maxbbb88@gmail.com>
2020-05-27 21:36:37 +0300

    editing login after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  before_action :load_user, except: [:index, :create, :new]
-  before_action :authorize_user, except: [:index, :new, :create, :show]
+  before_action :load_user, except: %i[index create new]
+  before_action :authorize_user, except: %i[index new create show]
 
   def index
     @users = User.all
@@ -16,15 +18,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = @user.id
+      log_in @user
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
     else
       render 'new'
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @user.update(user_params)
@@ -51,6 +52,10 @@ class UsersController < ApplicationController
 
   def load_user
     @user ||= User.find params[:id]
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
   end
 
   def user_params
