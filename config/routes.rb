e5d2364a13ca600da1f7b960c4f04930c2f5b4d1
manzhaimaksim <maxbbb88@gmail.com>
2020-06-23 22:28:00 +0300

    adding hashtags to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :users
   resources :questions, except: [:show, :new, :index]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :hashtags, only: [:show], param: :text
+
   get 'sign_up' => 'users#new'
   get 'log_out' => 'sessions#destroy'
   get 'log_in' => 'sessions#new'
