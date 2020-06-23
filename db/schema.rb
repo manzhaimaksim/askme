480f631dc76905ed365d949c0eddbbd37bbc8e2e
manzhaimaksim <maxbbb88@gmail.com>
2020-06-23 20:22:35 +0300

    adding model HashtagQuestion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_193554) do
+ActiveRecord::Schema.define(version: 2020_06_23_171313) do
+
+  create_table "hashtag_questions", force: :cascade do |t|
+    t.integer "hashtag_id"
+    t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hashtag_id"], name: "index_hashtag_questions_on_hashtag_id"
+    t.index ["question_id"], name: "index_hashtag_questions_on_question_id"
+  end
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["text"], name: "index_hashtags_on_text", unique: true
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
