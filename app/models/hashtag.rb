13f3bea9026221f4847fe87257f3392102e39055
manzhaimaksim <maxbbb88@gmail.com>
2020-06-23 22:28:40 +0300

    adding relation to models

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,3 +1,10 @@
 class Hashtag < ApplicationRecord
+  REGEXP = /#[[:word:]-]+/
+
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :questions, through: :hashtag_questions
+
+  scope :with_questions, -> { left_outer_joins(:hashtag_questions).where.not(hashtag_questions: {id: nil}) }
+
   validates :text, presence: true, uniqueness: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,21 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
-  validates :text,  length: { maximum: 255 },
-                    presence: true
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :hashtags, through: :hashtag_questions
+
+  validates :text, length: { maximum: 255 }, presence: true
+
+  after_commit :create_hashtags, on: [:create, :update]
+
+  private
+
+  def create_hashtags
+    self.hashtags =
+      "#{answer} #{text}".
+          downcase.
+          scan(Hashtag::REGEXP).
+          uniq.
+          map { |ht| Hashtag.find_or_create_by(text: ht.delete('#')) }
+  end
 end
